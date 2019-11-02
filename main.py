133096bf7a50c378391eee3b94b72e6818cbe244
Abhijit <abhi88mcet@gmail.com>
2019-11-02 14:57:18 +0530

    first committ

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import pickle
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn import feature_selection as f_select
+import os.path
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import cross_val_score
+# from sklearn.cross_validation import cross_val_score
+from sklearn import linear_model
+import numpy as np
+import pandas as pd
+from scipy import stats, integrate
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Lasso
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import string
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
